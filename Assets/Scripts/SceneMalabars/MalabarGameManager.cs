6a463660c9572f732f0c1033cea5127f27b4484d
Berruti23 <pabloberrutisauba@gmail.com>
2025-02-09 17:11:57 +0100

    Final Version

--- Assets/Scripts/SceneMalabars/MalabarGameManager.cs
+++ Assets/Scripts/SceneMalabars/MalabarGameManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
-public class MalabarGameManager : MonoBehaviour{
+public class MalabarGameManager : MonoBehaviour
+{
     public SpriteSpawner SpriteSpawner;
     public CameraShake CameraShake;
     public DecreasingBar DecreasingBar;
@@ -20,23 +22,57 @@ public class MalabarGameManager : MonoBehaviour{
     public int actualWrongGuesses = 0;
     public bool correctOrWrongAnswer = false;
     public bool end = false;
+    private GameObject spawnedIntro; // Referencia al prefab instanciado
+    public GameObject introPrefab; // Prefab que se spawnea con la animación
+    public GameObject outroPrefab; // Prefab que se spawnea con la animación
+    public Transform spawnPoint;
+    private bool tutorialDone = false;
+    public GameObject tutorial;
 
-    void Start(){
+    void Start()
+    {
 
-        charGenerator();
+        StartCoroutine(StartScene());
+
 
     }
+    IEnumerator StartScene()//Se llama desde Start() con StartCoroutine(StartScene())
+    {
+        // Espera hasta que el jugador presione Espacio
+        while (!Input.GetKeyDown(KeyCode.Space))
+        {
+            yield return null;
+        }
 
-    void Update(){
+        // Se ejecuta una vez cuando se rompe el bucle (cuando se presiona Espacio)
+        if (!tutorialDone)
+        {
+            tutorial.SetActive(false);
+            startAnimDoor();
+
+            yield return new WaitForSeconds(4.5f);  // Espera un tiempo antes de activar la piedra, no cambiar el tiempo
+                                                    //aqui ahora activas todos los objetos que hacen que tu juego funcione.
+            charGenerator();
+            tutorialDone = true;
+
+
+        }
+    }
+
+
+    void Update()
+    {
 
         // Si es presiona una tecla i no s'esta fent la comprovació entra en la comprovació
-        if (!isListening && !end) {
+        if (!isListening && !end && tutorialDone)
+        {
             StartCoroutine(CollectKeys());
         }
     }
 
     // Analitza totes les tecles que introdueix el usuari, bucle principal de comprovacio
-    private IEnumerator CollectKeys(){
+    private IEnumerator CollectKeys()
+    {
 
         bool wrongKey = false;
         isListening = true;
@@ -46,30 +82,36 @@ public class MalabarGameManager : MonoBehaviour{
         float startTime = Time.time;
         StartCoroutine(DecreasingBar.DecreaseBar(solvingTime));
 
-        while (Time.time - startTime < solvingTime){
+        while (Time.time - startTime < solvingTime)
+        {
 
-            if (Input.anyKeyDown){
+            if (Input.anyKeyDown)
+            {
 
                 string inputStr = Input.inputString;
 
-                if (!string.IsNullOrEmpty(inputStr)){
-                    
+                if (!string.IsNullOrEmpty(inputStr))
+                {
+
                     char pressedChar = char.ToUpper(inputStr[0]);
                     //Debug.Log("Tecla presionada: " + pressedChar);
                     counter++;
 
-                    if(actualCombination[counter] == pressedChar){
+                    if (actualCombination[counter] == pressedChar)
+                    {
 
                         CorrectKey(counter);
 
                     }
-                    else{
+                    else
+                    {
                         wrongKey = true;
                         WrongKey(counter);
                         break;
                     }
 
-                    if(counter >= actualCombination.Count - 1){
+                    if (counter >= actualCombination.Count - 1)
+                    {
 
                         CorrectAnswer();
                         isListening = false;
@@ -79,8 +121,9 @@ public class MalabarGameManager : MonoBehaviour{
             }
             yield return null;
         }
-        
-        if(!wrongKey){
+
+        if (!wrongKey)
+        {
             wrongKey = false;
             WrongAnswer();
         }
@@ -88,21 +131,24 @@ public class MalabarGameManager : MonoBehaviour{
         isListening = false;
     }
 
-    private void charGenerator(){
-        
+    private void charGenerator()
+    {
+
         actualCombination.Clear();
 
         int leters;
 
-        if(actualCorrectGuesses < 3){leters = 2;}
-        else if(actualCorrectGuesses < 5){leters = 3;}
-        else{leters = 4;}
+        if (actualCorrectGuesses < 3) { leters = 2; }
+        else if (actualCorrectGuesses < 5) { leters = 3; }
+        else { leters = 4; }
 
-        for (int i = 0; i < leters; i++){
+        for (int i = 0; i < leters; i++)
+        {
 
             int asciiInt = Random.Range(65, 90 + 1);
 
-            while(actualCombination.Contains((char)asciiInt)){
+            while (actualCombination.Contains((char)asciiInt))
+            {
                 asciiInt = Random.Range(65, 90 + 1);
             }
 
@@ -123,15 +169,16 @@ public class MalabarGameManager : MonoBehaviour{
         totalCorrectGuesses++;
         actualCorrectGuesses++;
         actualWrongGuesses = 0;
-        if(actualCorrectGuesses >= 7){
-            end = true;
-            Victory();
-            return;
-        }
+
 
         //SpriteSpawner.ClearAllSprites(); // Elimina todos los sprites
         charGenerator();
         //Debug.Log("¡Correcto! Has presionado la combinación correcta.");
+        if (actualCorrectGuesses >= 7)
+        {
+            end = true;
+            StartCoroutine(Victory());
+        }
     }
 
     private void WrongAnswer()
@@ -155,7 +202,7 @@ public class MalabarGameManager : MonoBehaviour{
     }
 
     private void CorrectKey(int index)
-    {   
+    {
         //Debug.Log("¡Correcto!");
         AudioManager.PlayTeclaCorrecta();
         if (index >= 0 && index < actualCombination.Count)
@@ -167,7 +214,7 @@ public class MalabarGameManager : MonoBehaviour{
 
     private void WrongKey(int index)
     {
-        
+
         WrongAnswer();
     }
 
@@ -182,25 +229,39 @@ public class MalabarGameManager : MonoBehaviour{
 
         // 2) Asignamos el sprite "estático"
         renderer.sprite = monoSprite;
-        
+
         // 3) Esperamos un tiempo
         yield return new WaitForSeconds(0.6f);
 
         // 4) Reactivamos el Animator para reanudar la animación
-        if(!end){
+        if (!end)
+        {
             animator.enabled = true;
         }
     }
 
-    public void Victory(){
+    IEnumerator Victory()
+    {
         Debug.Log("¡Has ganado!");
+        if (outroPrefab != null && spawnPoint != null)
+        {
+            spawnedIntro = Instantiate(outroPrefab, spawnPoint.position, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(1.3f);  // Esperar 0.3 segundos antes de cambiar de nuevo
+        SceneManager.LoadScene(2);
     }
 
-    public void Defeat(){
+    public void Defeat()
+    {
         Debug.Log("¡Has perdido!");
     }
 
-    public bool getCorrectOrWrongAnswer(){
+    public bool getCorrectOrWrongAnswer()
+    {
         return correctOrWrongAnswer;
     }
-}   
+    private void startAnimDoor()
+    {
+        spawnedIntro = Instantiate(introPrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
